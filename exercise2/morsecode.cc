723cc4cec9ae3a7bbc71d3f1f6a5cc1c13e1a20a
Dexz92 <emin.gigovic@gmail.com>
2016-01-28 23:53:34 +0100

    Part of exercise 2 done

--- exercise2/morsecode.cc
+++ exercise2/morsecode.cc
@@ -2,16 +2,45 @@
 
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <map>
 
 using namespace std;
 
 MorseCode::MorseCode() {
+	ifstream infile("morse.def");
+	string code;
+	char decode;
+	string line;
+	while(getline(infile,line)){
+		istringstream iss(line);
+		iss >> decode;
+		iss >> code;
+		myMap.insert(make_pair(decode,code));
+	}
 }
 
 string MorseCode::encode(const string& text) const {
-	return "";
+	string encodedText;
+	map<char,string>::iterator it;
+	for(char ch : text){
+		encodedText.append(myMap.find(ch)->second);
+	}
+	return encodedText;
 }
 
 string MorseCode::decode(const string& code) const {
-	return "";
+	string decodedWord;
+	string decodedText;
+	string temp;
+	istringstream iss(code);
+	while(iss >> decodedWord){
+		for(auto it = myMap.begin(); it != myMap.end(); ++it){
+			if(it->second == decodedWord){
+				temp = it->first;
+				decodedText.append(temp);
+			}
+		}
+	}
+	return decodedText;
 }
