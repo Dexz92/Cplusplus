9386f9533d6a7c2124ff8a3c782d7567b82d7aa9
Dexz92 <emin.gigovic@gmail.com>
2016-02-07 14:34:11 +0100

    Done with exercise3

--- exercise3/crypto.cc
+++ exercise3/crypto.cc
@@ -6,9 +6,21 @@
 using namespace std;
 
 string Crypto::encrypt(const string& text, unsigned int key) {
-	return "";
+	default_random_engine e(key);
+	uniform_int_distribution<unsigned> u(0,255);
+	string result(text.size(),' ');
+	for(size_t i = 0; i != text.size(); ++i){
+		result[i] = (text[i] + u(e)) % 256;
+	}
+	return result;
 }
 
 string Crypto::decrypt(const string& crypt, unsigned int key) {
-	return "";
+	default_random_engine e(key);
+	uniform_int_distribution<unsigned> u(0,255);
+	string result(crypt.size(),' ');
+	for(size_t i = 0; i != crypt.size(); ++i){
+		result[i] = (crypt[i] - u(e)) % 256;
+	}
+	return result;
 }
